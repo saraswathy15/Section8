577937bd7eb732fb17faaeb04113cfb436d8bfca
saraswathy15 <charusrinivasan15@gmail.com>
2024-01-03 16:14:18 +0530

    Add project files.

---
+++ AssisProSingleLinkedList/AssisProSingleLinkedList/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssisProSingleLinkedList
+{
+    public class Node
+    {
+        private string data;
+        private Node next = null;
+        public string Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        public Node Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+    }
+}
+
+    
